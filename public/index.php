bffa18df5f7127025eef89a237d29af458105969
Matt Ward <mattward@sparkcentral.com>
2016-06-13 10:11:15 -0700

    a simple CRUD

--- public/index.php
+++ public/index.php
@@ -1,13 +1,35 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+
+use \Symfony\Component\HttpFoundation\Request;
+use \Propel\Runtime\Map\TableMap;
+use \Symfony\Component\HttpKernel\HttpKernelInterface;
+
 $app = new Silex\Application();
 $app->register(new Propel\Silex\PropelServiceProvider(), array(
     'propel.config_file' => __DIR__.'/../config/config.php'
 ));
-$app->get('/hello/{name}', function ($name) use ($app) {
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../view',
+));
 
-    $q = new Packagerator\PackageQuery();
-    $firstPackage = $q->findPK(1);
-    return 'Hello '.$app->escape($name);
+$app->get('/info', function () use ($app) {
+    phpinfo();
 });
+
+$app->get('/package/{id}', function ($id) use ($app) {
+    return $app['twig']->render('package/view.twig', Packagerator\PackageQuery::create()->findPK($id)->toArray(TableMap::TYPE_FIELDNAME));
+});
+
+$app->get('/package', function () use ($app) {
+    return $app['twig']->render('package/form.twig');
+});
+
+$app->post('/package', function (Request $request) use ($app) {
+    $package = new \Packagerator\Package();
+    $package->fromArray($request->request->all(), TableMap::TYPE_FIELDNAME);
+    $package->save();
+    return $app->redirect('/package/'.$package->getId());
+});
+
 $app->run();
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ vendor/bin/propel build --output-dir src/model
 vendor/bin/propel config:convert --output-dir config
 vendor/bin/propel sql:build --output-dir sql --overwrite
 vendor/bin/propel sql:insert --sql-dir sql
+php -S localhost:8080 -c php.ini
